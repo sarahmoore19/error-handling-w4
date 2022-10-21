6caf60de148532260f7b2a98bea7478e2a08f47e
sarahmoore <sarahnmoore19@gmail.com>
2022-10-21 17:44:26 -0400

    solved

--- error-handling.js
+++ error-handling.js
@@ -7,20 +7,38 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+try {
+  let res = sum(null);
+  console.log(res);
+}
+catch(error) {
+  if (error instanceof TypeError) console.log(error.message);
+}
 
 // 2.
-// tests
+let sayName = name => {
+  if (typeof name === "string") console.log(name);
+  else throw new Error("Invalid name! Must be a string!");
+}
+try {
 sayName("Alex");
 sayName(1);
-// Your code here
+}
+catch(e) {
+  console.log(e.message);
+}
 
 // 3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
-
   console.log(greeting);
+}
+
+try {
+  greet("");
+}
+catch(e) {
+  console.log("Hello World");
 }
